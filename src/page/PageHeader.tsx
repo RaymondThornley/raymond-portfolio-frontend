d0d73d2105d8a5fb879bd7a34a2291eb52d177e9
RaymondThornley <Raymond@Thornley.net>
2020-04-24 14:02:22 -0500

    Fixed up header and main page styling

--- src/page/PageHeader.tsx
+++ src/page/PageHeader.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Navbar, NavLink } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 class PageHeader extends React.Component {
     render() {
         return (
-            <Navbar>
-                <LinkContainer to="/"><NavLink>Home</NavLink></LinkContainer>
-                <LinkContainer to="/mastermind"><NavLink>Mastermind</NavLink></LinkContainer>
-                <LinkContainer to="/minesweeper"><NavLink>Minesweeper</NavLink></LinkContainer>
-                <LinkContainer to="/tankwars"><NavLink>Tank Wars</NavLink></LinkContainer>
+            <Navbar expand="lg">
+                {/* <Navbar.Brand>Raymond Thornley</Navbar.Brand> */}
+                <Navbar.Toggle aria-controls="header-nav" />
+                <Navbar.Collapse id="header-nav">
+                    <Nav>
+                        <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+                        <LinkContainer to="/mastermind"><Nav.Link>Mastermind</Nav.Link></LinkContainer>
+                        <LinkContainer to="/minesweeper"><Nav.Link>Minesweeper</Nav.Link></LinkContainer>
+                        <LinkContainer to="/tankwars"><Nav.Link>Tank Wars</Nav.Link></LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         );
     }
