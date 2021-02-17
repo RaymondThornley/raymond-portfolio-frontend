f88206fa13f3ab2c6d20ab5c7259a19dff0da4f1
RaymondThornley <Raymond@Thornley.net>
2021-02-17 17:07:07 -0600

    Created Random AI for connect 5

--- src/connect/ConnectAi.ts
+++ src/connect/ConnectAi.ts
@@ -10,19 +10,38 @@ export type aiControls = {
     chooseSquare: (board: SpaceType[][]) => aiChosenSquare | null // Only should return null on error
 }
 
-const aiTest: aiControls = { // Remove when ai controls work and replace with better
-    name: "Test",
+// const aiTest: aiControls = {
+//     name: "Test",
+//     chooseSquare: function (board: SpaceType[][]) {
+//         for (let i = 0; i < board.length; i++) {
+//             for (let j = 0; j < board.length; j++) {
+//                 if (board[i][j].textValue === "Q") {
+//                     return { xPos: j, yPos: i }
+//                 }
+//             }
+//         }
+
+//         return null;
+//     }
+// }
+
+const aiRandom: aiControls = {
+    name: "Random",
     chooseSquare: function (board: SpaceType[][]) {
+        const squareList = [];
+
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board.length; j++) {
                 if (board[i][j].textValue === "Q") {
-                    return { xPos: j, yPos: i }
+                    squareList.push({ xPos: j, yPos: i });
                 }
             }
         }
 
-        return null;
+        const index = Math.floor(Math.random() * squareList.length);
+
+        return squareList[index];
     }
 }
 
-export const aiList = [aiTest];
+export const aiList = [aiRandom];
